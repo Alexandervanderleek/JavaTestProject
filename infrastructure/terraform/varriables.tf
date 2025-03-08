4cfd6f87886719bd97b9e41160b84abbdf8d7dc4
Alexander van der Leek <alexandervanderleek@gmail.com>
2025-03-08 20:14:12 +0200

    Changed default region

--- infrastructure/terraform/varriables.tf
+++ infrastructure/terraform/varriables.tf
@@ -2,7 +2,7 @@
 variable "aws_region" {
   description = "AWS region to deploy resources"
   type        = string
-  default     = "us-west-2"
+  default     = "eu-west-3"
 }
 
 variable "project_name" {
