2ca27f425084344f09f9d89e0fd988eb7b6abece
Alexander van der Leek <alexandervanderleek@gmail.com>
2025-03-09 09:33:17 +0200

    dns

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -21,6 +21,8 @@ provider "aws" {
 # Basic VPC setup
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
   
   tags = {
     Name = "${var.project_name}-vpc"
