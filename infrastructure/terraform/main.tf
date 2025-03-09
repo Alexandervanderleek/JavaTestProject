87a78f2a8f48e706ac8416d1b69e4ca8964aab09
Alexander van der Leek <alexandervanderleek@gmail.com>
2025-03-09 11:09:13 +0200

    make download public

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -138,3 +138,44 @@ resource "aws_ecr_repository" "app_repo" {
 resource "aws_s3_bucket" "cli_bucket" {
   bucket = "${var.project_name}-cli"
 }
+
+resource "aws_s3_bucket_public_access_block" "cli_bucket_public_access" {
+  bucket = aws_s3_bucket.cli_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_ownership_controls" "cli_bucket_ownership" {
+  bucket = aws_s3_bucket.cli_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "cli_bucket_acl" {
+  depends_on = [
+    aws_s3_bucket_public_access_block.cli_bucket_public_access,
+    aws_s3_bucket_ownership_controls.cli_bucket_ownership,
+  ]
+  bucket = aws_s3_bucket.cli_bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "cli_bucket_policy" {
+  bucket = aws_s3_bucket.cli_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.cli_bucket.arn}/*"
+      }
+    ]
+  })
+}
